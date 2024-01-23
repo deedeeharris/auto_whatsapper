ff945aa6142e64ec1a4119111c22cb12742ca554
deedeeharris <Yedidya.harris@mail.huji.ac.il>
2024-01-24 00:07:22 +0200

    Your commit message

--- Home.py
+++ Home.py
@@ -1,4 +1,39 @@
 import streamlit as st
+import subprocess
+import sys
+import os
+
+def update_script():
+    try:
+        # Get the script's directory
+        script_directory = os.path.dirname(os.path.realpath(__file__))
+
+        # Set the working directory to the script's directory
+        os.chdir(script_directory)
+
+        # Run git fetch to update remote branches
+        subprocess.run(['git', 'fetch'])
+
+        # Check if there are any changes
+        result = subprocess.run(['git', 'status', '-uno'], capture_output=True, text=True)
+
+        if "Your branch is behind" in result.stdout:
+            print("Updating script...")
+            subprocess.run(['git', 'pull'])
+            print("Script updated. Please restart the script.")
+            sys.exit(0)
+        else:
+            print("Your script is up to date.")
+
+    except Exception as e:
+        print(f"Error updating script: {e}")
+
+# Call the function to update the script
+update_script()
+
+
+
+
 
 st.set_page_config(
     page_title="Auto WhatApper - Home",
@@ -14,3 +49,5 @@ st.sidebar.success("Select an option above.")
 # instructions, in this app you can send messages to phone numbers or to contacts/groups
 st.write("### Instructions:")
 st.write('Choose an option from the sidebar on the left.')
+
+
